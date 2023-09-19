3f9832e0cfb24d41d50e0ff367cc7de33f65bf04
William Viana <willdev.viana@gmail.com>
2023-09-19 20:45:32 -0300

    understanding dictioraries

---
+++ example_07.py
@@ -0,0 +1,20 @@
+# Dictionaries
+bag = dict()
+
+bag['money'] = 12
+bag['candy'] = 5
+bag['keys'] = 4
+
+print(bag)
+print(bag['money'])
+
+bag['candy'] = bag['candy'] + 4
+
+print(bag)
+
+dictionarie = {"Fri": 20, "Thu": 6, "Sat": 1}
+dictionarie['Thu'] = 13
+dictionarie['Sat'] = 2
+dictionarie['Sun'] = 9
+
+print(dictionarie)
